3cba6756d7c18c0a232d1e3353d97b643cb0d2ff
Rasmus Ljungmann Pedersen <rasmuslp@gmail.com>
2014-10-02 23:34:05 +0000

    Can not correct the most common error from the data.

--- app.js
+++ app.js
@@ -20,10 +20,23 @@
          this.currentUsage = 'Collecting data...';
          this.plotData = [];
          this.plotOptions = {
+            axes: {
+              x: {
+                 labelFunction: function(value) {
+                    return value + ' s';
+                 }
+              },
+              y: {
+                 min: 0,
+                 labelFunction: function(value) {
+                    return value + ' W'
+                 }
+              }
+            },
             series: [{
-               y: 'value',
+               y: 'usage',
                color: 'steelblue',
-               thickness: '2px',
+               thickness: '1px',
                type: 'area',
                label: 'Watt'
             }],
@@ -32,7 +45,7 @@
             tooltip: {
                mode: 'scrubber',
                formatter: function(x, y, series) {
-                  return y + ' W';
+                  return y + ' W\n' + y + ' s';
                }
             },
             drawLegend: true,
@@ -41,34 +54,80 @@
          };
 
          this.plotTimeZero = 'undefined';
+
          var self = this;
+         var unverfiedUsage = false;
+
          this.data.$watch(function(event) {
             if (event.event === 'child_added') {
                // New data
+               self.data.$getRecord(event.key).verified = false;
 
                if (event.prevChild === null) {
                   return;
                }
 
-               var n = self.data.$getRecord(event.key);
-               var nMinus1 = self.data.$getRecord(event.prevChild);
+               // if (self.data.length < 4) {
+               //    return;
+               // }
 
-               self.currentUsage = self.calculateUsage(n, nMinus1);
+               var current = self.data.$getRecord(event.key);
+               var prev = self.data.$getRecord(event.prevChild);
+               var usage = self.calculateUsage(current, prev);
 
-               if (self.plotTimeZero === 'undefined') {
-                  self.plotTimeZero = n.timeStamp;
+               if (self.unverfiedUsage) {
+                  // Comparison of current usage vs last unverified usage
+                  
+                  //var lastUsage = self.unverfiedUsage;
+                  var lastVerifiedUsage = self.plotData[self.plotData.length - 1].usage;
+                  var diff = Math.abs(usage - lastVerifiedUsage);
+                  if (diff > 0.05* lastVerifiedUsage) {
+                     console.log('Alright, load must have changed');
+                     self.addDataToPlot(prev, self.unverfiedUsage);
+                  } else {
+                     console.log('Correcting data');
+                     self.addDataToPlot(prev, usage);
+                  }
+                  
+                  self.unverfiedUsage = false;
+               } else {
+                  //??
+
+                  if (self.plotData.length > 1) {
+                     // Comparison of current usage vs last verified usage
+                     var lastVerifiedUsage = self.plotData[self.plotData.length - 1].usage;
+                     var diff = Math.abs(usage - lastVerifiedUsage);
+                     if (diff > 0.05* lastVerifiedUsage) {
+                        // Max 10% diff filter
+                        console.log('Diff to big, not verified');
+                        self.unverfiedUsage = usage;
+                        return;
+                     }
+                  }
+                  
+                  //??
                }
 
-               self.plotData.push({
-                  x: Math.round((n.timeStamp - self.plotTimeZero) / 1000),
-                  value: self.currentUsage
-               });
-
-               //console.log(self.plotData);
+               // var currentM2 = self.data.$getRecord(self.data.$keyAt(self.data.$indexFor(event.prevChild)-1));
+               // var currentM3 = self.data.$getRecord(self.data.$keyAt(self.data.$indexFor(event.prevChild)-2));
+               //var usageM2 = self.calculateUsage(currentM2, currentM3);
+               
+               self.addDataToPlot(current, usage);
             }
          });
+         
+         this.addDataToPlot = function(dataPoint, usage) {
+            if (this.plotTimeZero === 'undefined') {
+                  this.plotTimeZero = dataPoint.timeStamp;
+               }
 
+               this.currentUsage = usage;
 
+               this.plotData.push({
+                  x: Math.round((dataPoint.timeStamp - this.plotTimeZero) / 1000),
+                  usage: usage
+               });
+         };
 
          this.calculateUsage = function(prev, current) {
             var time = prev.timeStamp - current.timeStamp;
