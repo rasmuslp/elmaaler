7ec548505e8de647d9e4582ea58ec7424283532d
Morten Ydefeldt <morten@ydefeldt.com>
2014-10-14 15:27:18 +0200

    re

--- phyton/main.py
+++ phyton/main.py
@@ -1,10 +1,41 @@
 from metaheuristic import hill_climber, simulated_annealing
-from metaheuristic.instance import Instance
+from firebase import Firebase
+import matplotlib.pyplot as plt
+import json
 
-for instance_no in [0, 1]:
-    for count in [3, 5, 10]:
-        instance = Instance( instance_no )
-        simulated_annealing.solve( instance, count )
-        print ""
-        print "------"
-        print ""
+f = Firebase('https://elmaaler.firebaseio.com/device/**********/data/incoming/raw2')
+r = f.get()
+d = []
+i = 0
+s = []
+prevTime = 0
+for k,v in iter(sorted(r.iteritems())):
+  #print v['timeDiff']
+  #print (v['timeDiff']-l)/1000
+  #print v
+  if (i == 1):
+    prevTime=v['timeDiff']
+  i = i + 1
+  if (prevTime != 0):
+    time = v['timeDiff'] - prevTime-1;
+    time = time / 1000.0;
+    usage = 3600 / time;
+    print usage
+    d.append(usage)
+  prevTime = v['timeDiff']
+
+for i in range(len(d)):
+  usage = d[i]
+  devicesUsage = simulated_annealing.solve( 1, 1, usage )
+  print devicesUsage
+  s.append(devicesUsage)
+
+
+
+plt.plot(d)
+plt.plot(s)
+print s
+
+plt.axis([0, 1500, 0, 3000])
+plt.ylabel('WATTS')
+plt.show()
